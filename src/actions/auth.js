fda024b58336ceb09993a1e2ff343fe1abf1125a
jcamacaro <jesus.alejandr@gmail.com>
2020-08-18 16:51:13 -0400

    swit alert

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2'
+
 import { types } from "../types/types"
 import {firebase, googleAuthProvider } from '../firebase/firebaseConfig'
 import { startLoading, finishLoading } from "./ui"
@@ -18,6 +20,8 @@ export const startLoginEmailPassword = (email, password) => {
       .catch( e => {
         console.log(e)
         dispatch( finishLoading() );
+
+        Swal.fire('Error', e.message, 'error' );
       })
 
     
@@ -39,7 +43,8 @@ export const startRegisterWithEmailPasswordName = ( email, password, name ) => {
         )
       })
       .catch( e => {
-        console.log(e)
+        console.log(e);
+        Swal.fire('Error', e.message, 'error' );
       })
   }
 }
