39195ec61893d09ff2463b478a206d300ff08805
jcamacaro <jesus.alejandr@gmail.com>
2020-08-19 09:24:19 -0400

    get notes

--- src/helpers/loadNotes.js
+++ src/helpers/loadNotes.js
@@ -7,7 +7,15 @@ export const loadNotes = async ( uid ) => {
   const notesSnap = await db.collection(`${ uid }/journal/notes`).get();
   const notes = [];
 
-  console.log(notesSnap);
+  notesSnap.forEach( snapHijo => {
+  // console.log(snapHijo.data())
+  // console.log(notesSnap);
+    notes.push({
+      id: snapHijo.id,
+      ...snapHijo.data()
+    })
+  })
 
+  // console.log(notes)
   return notes;
 }
