748a69617e3bd0d9b25a71da684e7f41695a5858
jcamacaro <jesus.alejandr@gmail.com>
2020-08-23 11:30:51 -0400

    notas test

--- src/__tests__/actions/notes.test.js
+++ src/__tests__/actions/notes.test.js
@@ -3,21 +3,46 @@ import '@testing-library/jest-dom'
 
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startNewNote, startDeleting } from '../../actions/notes';
+import { startNewNote, startDeleting, startLoadingNotes, startSaveNote, startUploading } from '../../actions/notes';
 import { types } from '../../types/types';
-// import { db } from '../../firebase/firebaseConfig';
+import { db } from '../../firebase/firebaseConfig';
+import { fileUpload } from '../../helpers/fileUpload';
+
+// Mock function con su respuesta 
+jest.mock('../../helpers/fileUpload', () => ({
+  fileUpload: jest.fn( () => {
+    // return 'https://hola-mundo.com/cosa.jpg' o
+    return Promise.resolve('https://hola-mundo.com/cosa.jpg')
+  } )
+}) );
  
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-// State de mi Redux en la app - lo que se ve en el Redux DevsTools
-const store = mockStore({
+const initState = {
   auth: {
     uid: 'TESTING'
+  },
+  notes: {
+    active: {
+      id: 'CPps6C3GqiWF1mj9CGku',
+      title: 'Hola 1',
+      body: 'Mundo 1'
+    }
   }
-});
+}
+
+// State de mi Redux en la app - lo que se ve en el Redux DevsTools
+let store = mockStore(initState);
 
 describe('Pruebas con las acciones de notes', () => {
+
+  beforeEach( () => {
+
+    // reinicializacion del store
+    store = mockStore(initState);
+
+  } )
   
   test('debe de crear una nueva nota startNewNote', async () => {
 
@@ -59,4 +84,58 @@ describe('Pruebas con las acciones de notes', () => {
     // Ö
     // await db.doc(`/TESTING/journal/notes/${docId}`).delete();
   })
+
+  test('startLoadingNotes deve cargar las notas ', async () => {
+    
+    await store.dispatch( startLoadingNotes( 'TESTING' ) );
+
+    const actions = store.getActions();
+
+    expect( actions[0] ).toEqual({
+      type: types.notesLoad,
+      payload: expect.any(Array)
+    })
+
+    const expected = {
+      id: expect.any(String),
+      title: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(Number),
+    }
+
+    // Evaluar estas propiedades que les paso
+    expect( actions[0].payload[0] ).toMatchObject(expected)
+  });
+
+  test('startSavenote debe de actualizar la nota', async () => {
+    
+    const note = {
+      id: 'BiCwMNr398hVpun5Mf7P',
+      title: 'titulo update',
+      body: 'body update'
+    }
+
+    await store.dispatch( startSaveNote( note ) )
+
+    const actions = store.getActions();
+
+    expect( actions[0].type ).toBe( types.notesUpdated );
+
+    const docRef = await db.doc(`/TESTING/journal/notes/${ note.id }`).get();
+
+    expect( docRef.data().title ).toBe( note.title );
+  });
+
+  test('startUploading debe de actualizar el url del entry ', async () => {
+    
+    const file = new File([], 'foto.jpg');
+
+    await store.dispatch( startUploading( file ) );
+
+    const docRef = await db.doc('/TESTING/journal/notes/CPps6C3GqiWF1mj9CGku').get();
+    expect( docRef.data().url ).toBe('https://hola-mundo.com/cosa.jpg');
+  })
+   
+  
+  
 })
--- src/setupTests.js
+++ src/setupTests.js
@@ -6,4 +6,8 @@ import {createSerializer} from 'enzyme-to-json';
 
 Enzyme.configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+
+// Saltarme un error
+const noScroll = () => {};
+Object.defineProperty( window, 'scrollTo', { value: noScroll, writable: true} );
  
